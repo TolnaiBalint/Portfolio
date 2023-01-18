d05a14c4d07f4ca257d0ba92c90d3e0925f32083
Blint <bliny073@gmail.com>
2023-01-18 11:22:29 +0100

    alap portfolio

---
+++ animation.js
@@ -0,0 +1,27 @@
+
+/*------------------------------------*\
+  BUTTON
+\*------------------------------------*/
+
+function gotoAbout() {
+    window.scroll({
+        top: 800,
+        left: 0,
+        behavior: "smooth",
+    });
+}
+
+
+
+
+/*------------------------------------*\
+  BACK-BUTTON
+\*------------------------------------*/
+
+function gotoTop() {
+    window.scroll({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+    });
+}
