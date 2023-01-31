d28aba1d9ec69a935506d1baeb237cbfe466f5b6
TolnaiBalint <bliny073@gmail.com>
2023-01-31 10:36:33 +0100

    max 992px animation slides in to the top.

--- animation.js
+++ animation.js
@@ -24,3 +24,55 @@ function gotoTop() {
         behavior: "smooth",
     });
 }
+
+
+
+/*------------------------------------*\
+  PROJECT
+\*------------------------------------*/
+
+/*-----------------*\
+  pc img
+\*-----------------*/
+
+const scrollElements = document.querySelectorAll(".js-scroll");
+
+const elementInView = (el, dividend = 1) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop <=
+    (window.innerHeight || document.documentElement.clientHeight) / dividend
+  );
+};
+
+const elementOutofView = (el) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop > (window.innerHeight || document.documentElement.clientHeight)
+  );
+};
+
+const displayScrollElement = (element) => {
+  element.classList.add("scrolled");
+};
+
+const hideScrollElement = (element) => {
+  element.classList.remove("scrolled");
+};
+
+const handleScrollAnimation = () => {
+  scrollElements.forEach((el) => {
+    if (elementInView(el, 1)) {
+      displayScrollElement(el);
+    } else if (elementOutofView(el)) {
+      hideScrollElement(el)
+    }
+  })
+}
+
+window.addEventListener("scroll", () => { 
+  handleScrollAnimation();
+});
+
